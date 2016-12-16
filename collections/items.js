0b72c311dbfc785319e505f8891019ae582ebdd1
Pavel Grechishkin <grechishkin.pavel@yandex.ru>
2016-12-17 01:40:40 +0300

    unread state for articles (without counters)

--- collections/items.js
+++ collections/items.js
@@ -12,10 +12,20 @@ define(['backbone', 'models/item'], function(Backbone, Item) {
 			this.url = options.url;
 			this.tree = options.tree;
 			this.settings = {};
-			this.seq_gh = 0;
+			this.seq_gh = 0; // getHeadlines seq
+			this.seq_cs = 0; // change state seq
+			// ---
+			this.ItemsToMarkRead = [];
+			this.ItemsToMarkUnread = [];
+			this.ItemsToMarkStar = [];
+			this.ItemsToMarkUnstar = [];
 			// ---
 			this.listenTo(this.tree, 'change:current', this.eChangeCurrent);
 			// ---
+			this.listenTo(this, 'change:unread', this.eStateUnread);
+			// ---
+			// init regular check of state changes every 2 seconds
+			setInterval(this.checkState.bind(this), 5 * 1000);
 
 		},
 		fetch: function() {
@@ -107,6 +117,92 @@ define(['backbone', 'models/item'], function(Backbone, Item) {
 			this.trigger('change:current', this.current);
 			// ---
 			// this.fetch();
+
+		},
+		eStateUnread: function(item) {
+
+			console.log(this.title + ": unread state changed for item " + item.sid());
+			if (item.get('unread')) {
+				this.ItemsToMarkUnread.push(item.id);
+			} else {
+				this.ItemsToMarkRead.push(item.id);
+			}
+
+		},
+		checkState: function() {
+
+			var sq = []; // state queue
+			// Unread state
+			var qmRead = this.ItemsToMarkRead.length;
+			var qmUnread = this.ItemsToMarkUnread.length;
+			if (qmRead != 0 || qmUnread != 0) {
+
+				if (qmRead != 0 && qmUnread == 0) {
+					sq.push({
+						state: 'unread',
+						value: false,
+						ids: this.ItemsToMarkRead.join()
+					});
+					this.ItemsToMarkRead.length = 0;
+				}
+				// ---
+				if (qmRead == 0 && qmUnread != 0) {
+					sq.push({
+						state: 'unread',
+						value: true,
+						ids: this.ItemsToMarkUnread.join()
+					});
+					this.ItemsToMarkUnread.length = 0;
+				}
+				// ---
+				if (qmRead != 0 && qmUnread != 0) {
+
+					// use underscore's difference()
+					sq.push({
+						state: 'unread',
+						value: false,
+						ids: _.difference(this.ItemsToMarkRead, this.ItemsToMarkUnread).join()
+					});
+					// ---
+					sq.push({
+						state: 'unread',
+						value: true,
+						ids: _.difference(this.ItemsToMarkUnread, this.ItemsToMarkRead).join()
+					});
+					// ---
+					this.ItemsToMarkRead.length = 0;
+					this.ItemsToMarkUnread.length = 0;
+
+				}
+
+			}
+			// -------------------------
+			_(sq).each((element) => {
+
+				this.seq_cs++;
+				// ---
+				var opts = {
+					"op": "updateArticle",
+					"sid": this.session,
+					"seq": this.seq_cs,
+				};
+				// ---
+				if (element.state == 'unread') {
+					opts["field"] = 2;
+					if (element.value) {
+						opts["mode"] = 1;
+					} else {
+						opts["mode"] = 0;
+					}
+					opts["article_ids"] = element.ids;
+				}
+				// ---
+				$.post(this.url, JSON.stringify(opts))
+					.done((result) => {
+						console.log(result);
+					});
+
+			});
 
 		}
 
